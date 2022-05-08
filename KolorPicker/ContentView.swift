dc507cd72d9d61d9b709f5f34b66bba9afcda05d
Pho Vu <phovu0703@gmail.com>
2022-05-07 23:46:39 -0400

    Initial Commit

---
+++ KolorPicker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  KolorPicker
+//
+//  Created by Hemin Vu on 5/7/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ KolorPicker/KolorPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  KolorPickerApp.swift
+//  KolorPicker
+//
+//  Created by Hemin Vu on 5/7/22.
+//
+
+import SwiftUI
+
+@main
+struct KolorPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
