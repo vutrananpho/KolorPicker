ff841ac29eb9e7d3c152bc736e4c6fa8259dcdec
Pho (Tran An) Vu / Haemin <phovu0703@gmail.com>
2022-05-08 02:15:17 -0400

    Update ContentView.swift

--- KolorPicker/ContentView.swift
+++ KolorPicker/ContentView.swift
@@ -1,16 +1,27 @@
 //
 //  ContentView.swift
-//  KolorPicker
+//  " KolorPicker" on Xcode utilizing SwiftUI package
 //
-//  Created by Hemin Vu on 5/7/22.
+//  Created by @Pho Vu on 5/7/22
+//  Inspired by YT Channel "Code Palace".
+//
 //
 
-import SwiftUI
+import SwiftUI // import Swift user interface framework
 
 struct ContentView: View {
+    @State private var color = Color.blue // set variable for 1st color = blue
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Circle() // initiate Circle() to draw a circle
+                .frame(width:200, height: 200) // customize frame
+                .foregroundColor(color) // set foreground color
+                .padding() // control spacing #1
+            
+            ColorPicker("Choose a color", selection: $color) // pick a color
+        }
+        .padding() // control spacing #2
     }
 }
 
